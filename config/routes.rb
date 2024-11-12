07d327a7c24ee3936436eebf318440c53533b6d6
Ryouga13 <184459763>
2024-11-12 11:12:52 +0000

    Finish 4章【詳細画面を作ろう】

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   get 'books/new'
   get 'books/create'
-  get 'books/index'
-  get 'books/show'
+  get 'books' => 'books#index'
+  get 'books/:id' => 'books#show', as: 'book'
   get 'books/edit'
-  get 'homes/top'
+  get 'top' => 'homes#top'
+
+  post 'books' => 'books#create'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
