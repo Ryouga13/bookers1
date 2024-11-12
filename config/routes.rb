e794510985b26ad6edb5a9ec0a305d3a505fb832
Ryouga13 <184459763>
2024-11-12 15:02:25 +0000

    Finish5章【編集機能を作ろう】

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   get 'books/new'
   get 'books/create'
-  get 'books' => 'books#index'
+  get 'books' => 'books#index',as: 'index'
   get 'books/:id' => 'books#show', as: 'book'
-  get 'books/edit'
+  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
   get 'top' => 'homes#top'
 
   post 'books' => 'books#create'
+  patch 'books/:id' => 'books#update', as: 'update_book'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
